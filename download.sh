84a2960053b383c625b7f68e02592d647b360726
RomanDesigner <46124855+RomanDesigner@users.noreply.github.com>
2019-03-31 16:31:45 +0300

    Update download.sh

--- download.sh
+++ download.sh
@@ -6,8 +6,7 @@ homedir="/root/"
 else
 homedir="/home/$USER/"
 fi
-mkdir "$homedir"lineage && cd "$homedir"lineage
-repo init -u git://github.com/iq4404/manifest-default.git -b cm-14.1 && repo sync
+mkdir "$homedir"lineage && cd "$homedir"lineage && repo init -u git://github.com/iq4404/manifest-default.git -b cm-14.1 && repo sync
 cp -r $patchespath/patches "$homedir"lineage/device/Fly/IQ4404/patches
 cd "$homedir"lineage/device/Fly/IQ4404/patches && chmod +x apply.sh && ./apply.sh
 echo "Your lineage OS in: "$homedir"lineage/"
